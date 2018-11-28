dec5e74cd28d0d24f98758bd3d1cb0ae2b81f717
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2018-11-28 21:15:59 +0100

    Fixes DISABLE_SWIZZLING compilation errors.

--- RxCocoa/Runtime/_RXObjCRuntime.m
+++ RxCocoa/Runtime/_RXObjCRuntime.m
@@ -16,12 +16,84 @@
 #import "include/_RX.h"
 #import "include/_RXObjCRuntime.h"
 
+// self + cmd
+#define HIDDEN_ARGUMENT_COUNT   2
+
+BOOL RX_is_method_with_description_void(struct objc_method_description method) {
+    return strncmp(method.types, @encode(void), 1) == 0;
+}
+
+id __nonnull RX_extract_argument_at_index(NSInvocation * __nonnull invocation, NSUInteger index) {
+    const char *argumentType = [invocation.methodSignature getArgumentTypeAtIndex:index];
+
+#define RETURN_VALUE(type) \
+    else if (strcmp(argumentType, @encode(type)) == 0) {\
+        type val = 0; \
+        [invocation getArgument:&val atIndex:index]; \
+        return @(val); \
+    }
+
+    // Skip const type qualifier.
+    if (argumentType[0] == 'r') {
+        argumentType++;
+    }
+
+    if (strcmp(argumentType, @encode(id)) == 0
+        || strcmp(argumentType, @encode(Class)) == 0
+        || strcmp(argumentType, @encode(void (^)(void))) == 0
+    ) {
+        __unsafe_unretained id argument = nil;
+        [invocation getArgument:&argument atIndex:index];
+        return argument;
+    }
+    RETURN_VALUE(char)
+    RETURN_VALUE(short)
+    RETURN_VALUE(int)
+    RETURN_VALUE(long)
+    RETURN_VALUE(long long)
+    RETURN_VALUE(unsigned char)
+    RETURN_VALUE(unsigned short)
+    RETURN_VALUE(unsigned int)
+    RETURN_VALUE(unsigned long)
+    RETURN_VALUE(unsigned long long)
+    RETURN_VALUE(float)
+    RETURN_VALUE(double)
+    RETURN_VALUE(BOOL)
+    RETURN_VALUE(const char *)
+    else {
+        NSUInteger size = 0;
+        NSGetSizeAndAlignment(argumentType, &size, NULL);
+        NSCParameterAssert(size > 0);
+        uint8_t data[size];
+        [invocation getArgument:&data atIndex:index];
+
+        return [NSValue valueWithBytes:&data objCType:argumentType];
+    }
+}
+
+NSArray *RX_extract_arguments(NSInvocation *invocation) {
+    NSUInteger numberOfArguments = invocation.methodSignature.numberOfArguments;
+    NSUInteger numberOfVisibleArguments = numberOfArguments - HIDDEN_ARGUMENT_COUNT;
+
+    NSCParameterAssert(numberOfVisibleArguments >= 0);
+
+    NSMutableArray *arguments = [NSMutableArray arrayWithCapacity:numberOfVisibleArguments];
+
+    for (NSUInteger index = HIDDEN_ARGUMENT_COUNT; index < numberOfArguments; ++index) {
+        [arguments addObject:RX_extract_argument_at_index(invocation, index) ?: [NSNull null]];
+    }
+
+    return arguments;
+}
+
+BOOL RX_is_method_signature_void(NSMethodSignature * __nonnull methodSignature) {
+    const char *methodReturnType = methodSignature.methodReturnType;
+    return strcmp(methodReturnType, @encode(void)) == 0;
+}
+
 #if !DISABLE_SWIZZLING
 
 #define NSErrorParam NSError *__autoreleasing __nullable * __nullable
-
-// self + cmd
-#define HIDDEN_ARGUMENT_COUNT   2
 
 @class RXObjCRuntime;
 
@@ -128,78 +200,6 @@ static BOOL RX_method_has_supported_return_type(Method method) {
 SEL __nonnull RX_selector(SEL __nonnull selector) {
     NSString *selectorString = NSStringFromSelector(selector);
     return NSSelectorFromString([RX_PREFIX stringByAppendingString:selectorString]);
-}
-
-BOOL RX_is_method_signature_void(NSMethodSignature * __nonnull methodSignature) {
-    const char *methodReturnType = methodSignature.methodReturnType;
-    return strcmp(methodReturnType, @encode(void)) == 0;
-}
-
-BOOL RX_is_method_with_description_void(struct objc_method_description method) {
-    return strncmp(method.types, @encode(void), 1) == 0;
-}
-
-id __nonnull RX_extract_argument_at_index(NSInvocation * __nonnull invocation, NSUInteger index) {
-    const char *argumentType = [invocation.methodSignature getArgumentTypeAtIndex:index];
-    
-#define RETURN_VALUE(type) \
-    else if (strcmp(argumentType, @encode(type)) == 0) {\
-        type val = 0; \
-        [invocation getArgument:&val atIndex:index]; \
-        return @(val); \
-    }
-
-    // Skip const type qualifier.
-    if (argumentType[0] == 'r') {
-        argumentType++;
-    }
-    
-    if (strcmp(argumentType, @encode(id)) == 0
-        || strcmp(argumentType, @encode(Class)) == 0
-        || strcmp(argumentType, @encode(void (^)(void))) == 0
-    ) {
-        __unsafe_unretained id argument = nil;
-        [invocation getArgument:&argument atIndex:index];
-        return argument;
-    }
-    RETURN_VALUE(char)
-    RETURN_VALUE(short)
-    RETURN_VALUE(int)
-    RETURN_VALUE(long)
-    RETURN_VALUE(long long)
-    RETURN_VALUE(unsigned char)
-    RETURN_VALUE(unsigned short)
-    RETURN_VALUE(unsigned int)
-    RETURN_VALUE(unsigned long)
-    RETURN_VALUE(unsigned long long)
-    RETURN_VALUE(float)
-    RETURN_VALUE(double)
-    RETURN_VALUE(BOOL)
-    RETURN_VALUE(const char *)
-    else {
-        NSUInteger size = 0;
-        NSGetSizeAndAlignment(argumentType, &size, NULL);
-        NSCParameterAssert(size > 0);
-        uint8_t data[size];
-        [invocation getArgument:&data atIndex:index];
-        
-        return [NSValue valueWithBytes:&data objCType:argumentType];
-    }
-}
-
-NSArray *RX_extract_arguments(NSInvocation *invocation) {
-    NSUInteger numberOfArguments = invocation.methodSignature.numberOfArguments;
-    NSUInteger numberOfVisibleArguments = numberOfArguments - HIDDEN_ARGUMENT_COUNT;
-    
-    NSCParameterAssert(numberOfVisibleArguments >= 0);
-    
-    NSMutableArray *arguments = [NSMutableArray arrayWithCapacity:numberOfVisibleArguments];
-    
-    for (NSUInteger index = HIDDEN_ARGUMENT_COUNT; index < numberOfArguments; ++index) {
-        [arguments addObject:RX_extract_argument_at_index(invocation, index) ?: [NSNull null]];
-    }
-    
-    return arguments;
 }
 
 void * __nonnull RX_reference_from_selector(SEL __nonnull selector) {
--- RxCocoa/Runtime/include/_RXObjCRuntime.h
+++ RxCocoa/Runtime/include/_RXObjCRuntime.h
@@ -8,6 +8,15 @@
 
 #import <Foundation/Foundation.h>
 
+/// Returns `YES` in case method has `void` return type.
+BOOL RX_is_method_with_description_void(struct objc_method_description method);
+
+/// Extracts arguments for `invocation`.
+NSArray * __nonnull RX_extract_arguments(NSInvocation * __nonnull invocation);
+
+/// Returns `YES` in case methodSignature has `void` return type.
+BOOL RX_is_method_signature_void(NSMethodSignature * __nonnull methodSignature);
+
 #if !DISABLE_SWIZZLING
 
 /**
@@ -86,15 +95,6 @@ void * __nonnull RX_reference_from_selector(SEL __nonnull selector);
 
 /// Ensures interceptor is installed on target object.
 IMP __nullable RX_ensure_observing(id __nonnull target, SEL __nonnull selector, NSError *__autoreleasing __nullable * __nullable error);
-
-/// Extracts arguments for `invocation`.
-NSArray * __nonnull RX_extract_arguments(NSInvocation * __nonnull invocation);
-
-/// Returns `YES` in case method has `void` return type.
-BOOL RX_is_method_with_description_void(struct objc_method_description method);
-
-/// Returns `YES` in case methodSignature has `void` return type.
-BOOL RX_is_method_signature_void(NSMethodSignature * __nonnull methodSignature);
 
 /// Default value for `RXInterceptionObserver.targetImplementation`.
 IMP __nonnull RX_default_target_implementation(void);
