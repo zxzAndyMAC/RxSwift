fae040ba5a2c05def8596b31e510838c2c28b85e
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2019-01-21 10:03:18 +0100

    Replace `AtomicInt` with mutex implementation.

--- Platform/AtomicInt.swift
+++ Platform/AtomicInt.swift
@@ -5,29 +5,61 @@
 //  Created by Krunoslav Zaher on 10/28/18.
 //  Copyright © 2018 Krunoslav Zaher. All rights reserved.
 //
+import Dispatch
 
-import RxAtomic
+final class AtomicInt {
+    private var mutex: pthread_mutex_t
+    private var value: Int32
 
-typealias AtomicInt = RxAtomic.AtomicInt
-
-extension AtomicInt {
     init(_ initialValue: Int32) {
-        self.init()
-        self.initialize(initialValue)
+        self.value = initialValue
+
+        var attr = pthread_mutexattr_t()
+        pthread_mutexattr_init(&attr); defer { pthread_mutexattr_destroy(&attr) }
+
+        self.mutex = pthread_mutex_t()
+        guard pthread_mutex_init(&self.mutex, &attr) == 0 else { fatalError() }
     }
 
     @discardableResult
-    mutating func increment() -> Int32 {
-        return self.add(1)
+    func increment() -> Int32 {
+        guard pthread_mutex_lock(&self.mutex) == 0 else { fatalError() }
+        let oldValue = self.value
+        self.value += 1
+        guard pthread_mutex_unlock(&self.mutex) == 0 else { fatalError() }
+        return oldValue
     }
 
     @discardableResult
-    mutating func decrement() -> Int32 {
-        return self.sub(1)
+    func decrement() -> Int32 {
+        guard pthread_mutex_lock(&self.mutex) == 0 else { fatalError() }
+        let oldValue = self.value
+        self.value -= 1
+        guard pthread_mutex_unlock(&self.mutex) == 0 else { fatalError() }
+        return oldValue
     }
 
-    mutating func isFlagSet(_ mask: Int32) -> Bool {
-        return (self.load() & mask) != 0
+    func isFlagSet(_ mask: Int32) -> Bool {
+        guard pthread_mutex_lock(&self.mutex) == 0 else { fatalError() }
+        let oldValue = self.value
+        guard pthread_mutex_unlock(&self.mutex) == 0 else { fatalError() }
+        return oldValue & mask != 0
+    }
+
+    @discardableResult
+    func fetchOr(_ mask: Int32) -> Int32 {
+        guard pthread_mutex_lock(&self.mutex) == 0 else { fatalError() }
+        let oldValue = self.value
+        self.value |= mask
+        guard pthread_mutex_unlock(&self.mutex) == 0 else { fatalError() }
+        return oldValue
+    }
+
+    func load() -> Int32 {
+        guard pthread_mutex_lock(&self.mutex) == 0 else { fatalError() }
+        let oldValue = self.value
+        guard pthread_mutex_unlock(&self.mutex) == 0 else { fatalError() }
+        return oldValue
     }
 }
 
